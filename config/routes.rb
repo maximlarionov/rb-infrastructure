17a98e0ad1c1db440663e56d34aa7f216e2efab7
maximlarionov <maxim.larionov@flatstack.com>
2016-08-01 01:03:18 +0300

    Introduce feedback form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "pages#home"
+
+  resource :feedback, only: %i(new create)
 end
